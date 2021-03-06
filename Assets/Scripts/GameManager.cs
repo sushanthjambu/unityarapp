32eee5efe4210e7f9216e28bdd442f5e9f0c6ee6
jambusushanth <jambusushanth@gmail.com>
2021-03-06 13:28:44 +0530

    Added Game Manager Script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public enum GameState
+    {
+        HOME,
+        VIEWER,
+        EDITOR
+    }
+
+    private string _currentScene = string.Empty;
+
+    GameState _currentGameState = GameState.HOME;
+
+    public GameState CurrentGameState
+    {
+        get { return _currentGameState; }
+        set { _currentGameState = value; }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get { return _instance; }
+    }
+
+    protected virtual void Awake()
+    {
+        if(_instance != null)
+        {
+            Debug.LogError("[Singleton] Trying to create another instance of a singleton class.");
+        }
+        else
+        {
+            _instance = (T)this;
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if(_instance == this)
+        {
+            _instance = null;
+        }
+    }
+}
