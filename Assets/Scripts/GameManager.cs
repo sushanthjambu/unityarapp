b08f707269d09ded40fa3c0e09e35f9651d430ff
jambusushanth <jambusushanth@gmail.com>
2021-03-12 15:07:59 +0530

    Added Viewer Scene with ARFoundation Packages.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
     public enum GameState
     {
-        HOME,
-        VIEWER,
-        EDITOR
+        Home,
+        Viewer,
+        Editor
     }
 
-    private string _currentScene = string.Empty;
-
-    GameState _currentGameState = GameState.HOME;
+    GameState _currentGameState = GameState.Home;
 
     public GameState CurrentGameState
     {
@@ -24,6 +23,26 @@ public class GameManager : Singleton<GameManager>
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadLevel(GameState gameState)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(gameState.ToString(), LoadSceneMode.Additive);
+        if (ao == null)
+        {
+            Debug.LogError("[Scene Manager] Unable to load Level " + gameState.ToString());
+            return;
+        }
+    }
+
+    public void UnloadLevel(GameState gameState)
+    {
+        AsyncOperation ao = SceneManager.UnloadSceneAsync(gameState.ToString());
+        if (ao == null)
+        {
+            Debug.LogError("[Scene Manager] Unable to unload Level " + gameState.ToString());
+            return;
+        }
     }
 
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : Singleton<UIManager>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnOptionsClick()
+    {
+        GameManager.Instance.LoadLevel(GameManager.GameState.Viewer);
+    }
+}
